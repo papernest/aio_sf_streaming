ac4b766b9f8f36d641f6709f8391d315879e8bb2
Christophe Gabard <christophe.gabard@papernest.com>
2018-03-16 11:29:04 +0100

    remove doc build

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to aio-sf-streaming\u2019s documentation!"],titleterms:{aio:0,document:0,indic:0,stream:0,tabl:0,welcom:0}})
